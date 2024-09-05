e2952771192d6d6bd5b6a59bd9ad617df315b55d
Reddy9100 <129190164+Reddy9100@users.noreply.github.com>
2024-09-05 16:46:14 +0530

    changed carousel

--- src/components/homescreen/GenreSection.jsx
+++ src/components/homescreen/GenreSection.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const GenreSection = () => {
-  const [activeGenre, setActiveGenre] = useState(''); // State to track the active button
+  const [activeGenre, setActiveGenre] = useState('All'); // Default 'All' is active
 
   const genres = [
     'All', 'Action', 'Comedy', 'Drama', 'Romance', 'Sports', 'Fantasy',
@@ -13,17 +13,17 @@ const GenreSection = () => {
   };
 
   return (
-    <div className=' p-4 pb-32' style={{backgroundColor : "#0E0E12"}}>
+    <div className='p-4 pb-32' style={{ backgroundColor: "#0E0E12" }}>
       <div className='flex justify-between items-center text-[#ECEDF0] py-4'>
         <h1 className='text-[18px] font-semibold text-[#ECEDF0]'>Genres Junction</h1>
       </div>
-      <div className='flex gap-[7px] overflow-x-scroll  scrollbar-hide overflow-y-hidden  mb-4'>
-      {genres.map((genre, index) => (
+      <div className='flex gap-[7px] overflow-x-scroll scrollbar-hide overflow-y-hidden mb-4'>
+        {genres.map((genre, index) => (
           <div key={index} className='h-[30px] w-[120px] flex items-center justify-center'>
             <button
               onClick={() => handleGenreClick(genre)}
               className={`text-sm px-3 font-semibold w-full h-full flex items-center justify-center 
-                ${index === 0 ? 'bg-[#ECEDF0] text-[#222227]' : activeGenre === genre ? 'bg-[#ECEDF0] text-[#222227]' : 'bg-[#222227] text-[#6A7185]'}
+                ${activeGenre === genre ? 'bg-[#ECEDF0] text-[#222227]' : 'bg-[#222227] text-[#6A7185]'}
               `}
             >
               {genre}
@@ -32,22 +32,22 @@ const GenreSection = () => {
         ))}
       </div>
       <div className='grid grid-cols-3 gap-[7px]'>
-        <img src="https://iansportalimages.s3.amazonaws.com/images/202310144069407.jpeg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0 ' loading='lazy'/>
-        <img src="https://static-koimoi.akamaized.net/wp-content/new-galleries/2023/07/bholaa-shankar-001.jpg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy'/>
-        <img src="https://wallpaperaccess.com/full/9041172.jpg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy'/>
-        <img src="https://th.bing.com/th/id/OIP.C9Zt_s2ppySfjKwgwurgjwHaMu?rs=1&pid=ImgDetMain" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy'/>
-        <img src="https://image.tmdb.org/t/p/original/6S4BRlZPfzrludhsPDPVe6fEoxV.jpg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy'/>
-        <img src="https://th.bing.com/th/id/OIP.dqkUusnLGPoZNDmpzmb73QAAAA?rs=1&pid=ImgDetMain" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy'/>
-        <img src="https://iansportalimages.s3.amazonaws.com/images/202310144069407.jpeg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy'/>
-        <img src="https://static-koimoi.akamaized.net/wp-content/new-galleries/2023/07/bholaa-shankar-001.jpg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy'/>
-        <img src="https://wallpaperaccess.com/full/9041172.jpg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy'/>
-        <img src="https://th.bing.com/th/id/OIP.C9Zt_s2ppySfjKwgwurgjwHaMu?rs=1&pid=ImgDetMain" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy'/>
-        <img src="https://image.tmdb.org/t/p/original/6S4BRlZPfzrludhsPDPVe6fEoxV.jpg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy'/>
-        <img src="https://th.bing.com/th/id/OIP.dqkUusnLGPoZNDmpzmb73QAAAA?rs=1&pid=ImgDetMain" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy'/>
-        <img src="https://iansportalimages.s3.amazonaws.com/images/202310144069407.jpeg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy'/>
-        <img src="https://static-koimoi.akamaized.net/wp-content/new-galleries/2023/07/bholaa-shankar-001.jpg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy'/>
+        
+        <img src="https://iansportalimages.s3.amazonaws.com/images/202310144069407.jpeg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy' />
+        <img src="https://static-koimoi.akamaized.net/wp-content/new-galleries/2023/07/bholaa-shankar-001.jpg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy' />
+        <img src="https://wallpaperaccess.com/full/9041172.jpg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy' />
+        <img src="https://iansportalimages.s3.amazonaws.com/images/202310144069407.jpeg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy' />
+        <img src="https://static-koimoi.akamaized.net/wp-content/new-galleries/2023/07/bholaa-shankar-001.jpg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy' />
+        <img src="https://wallpaperaccess.com/full/9041172.jpg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy' />
+        <img src="https://iansportalimages.s3.amazonaws.com/images/202310144069407.jpeg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy' />
+        <img src="https://static-koimoi.akamaized.net/wp-content/new-galleries/2023/07/bholaa-shankar-001.jpg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy' />
+        <img src="https://wallpaperaccess.com/full/9041172.jpg" alt="pic1" className='w-[102px] h-[144px] flex-shrink-0' loading='lazy' />
+        
+        
       </div>
-      <button className='bg-[#222227] text-[#6A7185] w-full font-normal  mt-4 p-2' style={{fontSize : "14px" ,fontWeight: "500"}}>View More</button>
+      <button className='bg-[#222227] text-[#6A7185] w-full font-normal mt-4 p-2' style={{ fontSize: "14px", fontWeight: "500" }}>
+        View More
+      </button>
     </div>
   );
 }
--- src/components/homescreen/HomeScreen.jsx
+++ src/components/homescreen/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Carousel } from 'antd';
+import { Carousel } from "@material-tailwind/react";
 import NowOnScreen from './NowOnScreen';
 import TopRated from './TopRated';
 import ScreeningSoon from './ScreeningSoon';
@@ -8,153 +8,166 @@ import DigitalPlatform from './DigitalPlatfoem'; // Fixed typo in the import
 import GenreSection from './GenreSection';
 import pic from '../../assets/gk1.jpg';
 import logo from '../../assets/title.png';
-import notify from "../../assets/notify.svg"
-
-import wish from "../../assets/wish.svg"
-import unwish from "../../assets/unwish.svg"
+import notify from "../../assets/notify.svg";
+import wish from "../../assets/wish.svg";
+import unwish from "../../assets/unwish.svg";
 
 const HomeScreen = () => {
   return (
     <>
-    <div style={{background:"#0E0E12"}}>
-      <Carousel
-        autoplay
-        dots={true}
-
-        className="rounded-full shadow-lg"
-      >
-        <div className="relative h-[360px] w-full">
-          <div
-            className="absolute inset-0 bg-cover bg-center"
-            style={{
-              backgroundImage: `linear-gradient(180deg, rgba(14, 14, 18, 0) 0%, rgba(14, 14, 18, 0.91) 79%, #0E0E12 94%), url(${pic})`,
-            }}
-            
-          />
-          <div className="flex flex-col justify-end items-center h-full relative z-10 pb-4">
-            <img src={logo} alt="logo" className="w-auto h-auto max-w-xs" />
-            <ul className="flex text-xs justify-around items-center pb-3 space-x-4  font-bold text-[#C5C8D0]">
-              <li className="relative ">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2 before:text-green-500 before:font-bold before:shadow-md before:text-lg">
-                  Soon Up
-                </span>
-              </li>
-              <li className="relative">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
-                  Romance
-                </span>
-              </li>
-              <li className="relative">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
-                  Scifi
-                </span>
-              </li>
-              <li className="relative">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
-                  Musical
-                </span>
-              </li>
-              <li className="relative bg-[#2B2D34] font-semibold  h-[18px] w-[30px]  flex justify-center items-center   rounded-sm">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
-                  U/A
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="relative h-[360px] w-full">
-          <div
-            className="absolute inset-0 bg-cover bg-center"
-            style={{
-              backgroundImage: `linear-gradient(180deg, rgba(14, 14, 18, 0) 0%, rgba(14, 14, 18, 0.91) 79%, #0E0E12 94%), url(${pic})`,
-            }}
-          />
-          <div className="flex flex-col justify-end items-center h-full relative z-10 pb-4">
-            <img src={logo} alt="logo" className="w-auto h-auto max-w-xs" />
-            <ul className="flex justify-center pb-3 space-x-4 text-xs font-medium text-[#C5C8D0]">
-              <li className="relative ">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2 before:text-green-500 before:font-bold before:shadow-md before:text-lg">
-                  Soon Up
-                </span>
-              </li>
-              <li className="relative">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
-                  Romance
-                </span>
-              </li>
-              <li className="relative">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
-                  Scifi
-                </span>
-              </li>
-              <li className="relative">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
-                  Musical
-                </span>
-              </li>
-              <li className="relative bg-[#2B2D34] font-semibold   h-[18px] w-[30px] flex justify-center items-center  rounded-sm">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
-                  U/A
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="relative h-[360px] w-full">
-          <div
-            className="absolute inset-0 bg-cover bg-center"
-            style={{
-              backgroundImage: `linear-gradient(180deg, rgba(14, 14, 18, 0) 0%, rgba(14, 14, 18, 0.91) 79%, #0E0E12 94%), url(${pic})`,
-            }}
-          />
-          <div className="flex flex-col justify-end items-center h-full relative z-10 pb-4">
-            <img src={logo} alt="logo" className="w-auto h-auto max-w-xs" />
-            <ul className="flex justify-center pb-3 space-x-4 text-xs font-medium text-[#C5C8D0]">
-              <li className="relative ">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2 before:text-green-500 before:font-bold before:shadow-md before:text-lg">
-                  Soon Up
-                </span>
-              </li>
-              <li className="relative">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
-                  Romance
-                </span>
-              </li>
-              <li className="relative">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
-                  Scifi
-                </span>
-              </li>
-              <li className="relative">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
-                  Musical
-                </span>
-              </li>
-              <li className="relative bg-[#2B2D34] font-semibold  h-[18px] w-[30px] flex justify-center items-center  rounded-sm">
-                <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2 ">
-                  U/A
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </Carousel>
-      
-        <div className="flex mx-auto w-[320px] text-[14px] h-[48px] gap-2 p-3 mt-4">
-            <div className="flex justify-center items-center w-[260px] h-[48px]  bg-[#222227] rounded-md">
-                <img src={notify} className='text-[#C5C8D0] text-md font-bold'/>
-                <p className='text-[#C5C8D0]  font-semibold ml-2'>Notify Me</p>
+      <div style={{ background: "#0E0E12" }} className=''>
+        <Carousel
+          autoplay
+         
+          loop
+          className=" shadow-lg overflow-x-hidden"
+          navigation={({ setActiveIndex, activeIndex }) => (
+            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2">
+              {/* Custom navigation dots */}
+              {Array.from({ length: 3 }).map((_, i) => (
+                <span
+                  key={i}
+                  onClick={() => setActiveIndex(i)}
+                  className={`w-[6px] h-[6px] mx-[2px] inline-block rounded-full ${
+                    activeIndex === i ? 'bg-[#C5C8D0]' : 'bg-[#3F465D]'
+                  }`}
+                ></span>
+              ))}
             </div>
-            <div className="p-3 bg-[#222227] w-[15vw] h-[48px] flex justify-center items-center text-[#C5C8D0] rounded-md font-bold">
-              <button><img src={wish}/></button>
+          )}
+        >
+          <div className="relative h-[360px] w-full">
+            <div
+              className="absolute inset-0 bg-cover bg-center"
+              style={{
+                backgroundImage: `linear-gradient(180deg, rgba(14, 14, 18, 0) 0%, rgba(14, 14, 18, 0.91) 79%, #0E0E12 94%), url(${pic})`,
+              }}
+            />
+            <div className="flex flex-col justify-end items-center h-full relative z-10 pb-4">
+              <img src={logo} alt="logo" className="w-auto h-auto max-w-xs" />
+              <ul className="flex text-xs justify-around items-center pb-5 space-x-4 font-bold text-[#C5C8D0]">
+                <li className="relative ">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2 before:text-green-500">
+                    Soon Up
+                  </span>
+                </li>
+                <li className="relative">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
+                    Romance
+                  </span>
+                </li>
+                <li className="relative">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
+                    Scifi
+                  </span>
+                </li>
+                <li className="relative">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
+                    Musical
+                  </span>
+                </li>
+                <li className="relative bg-[#2B2D34] font-semibold h-[18px] w-[30px] flex justify-center items-center rounded-sm">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
+                    U/A
+                  </span>
+                </li>
+              </ul>
             </div>
-        </div>
-        <br/>
+          </div>
 
-    </div>
-      
+          <div className="relative h-[360px] w-full">
+            <div
+              className="absolute inset-0 bg-cover bg-center"
+              style={{
+                backgroundImage: `linear-gradient(180deg, rgba(14, 14, 18, 0) 0%, rgba(14, 14, 18, 0.91) 79%, #0E0E12 94%), url(${pic})`,
+              }}
+            />
+            <div className="flex flex-col justify-end items-center h-full relative z-10 pb-4">
+              <img src={logo} alt="logo" className="w-auto h-auto max-w-xs" />
+              <ul className="flex text-xs justify-around items-center pb-5 space-x-4 font-bold text-[#C5C8D0]">
+              <li className="relative ">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2 before:text-green-500">
+                    Soon Up
+                  </span>
+                </li>
+                <li className="relative">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
+                    Romance
+                  </span>
+                </li>
+                <li className="relative">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
+                    Scifi
+                  </span>
+                </li>
+                <li className="relative">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
+                    Musical
+                  </span>
+                </li>
+                <li className="relative bg-[#2B2D34] font-semibold h-[18px] w-[30px] flex justify-center items-center rounded-sm">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
+                    U/A
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="relative h-[360px] w-full">
+            <div
+              className="absolute inset-0 bg-cover bg-center"
+              style={{
+                backgroundImage: `linear-gradient(180deg, rgba(14, 14, 18, 0) 0%, rgba(14, 14, 18, 0.91) 79%, #0E0E12 94%), url(${pic})`,
+              }}
+            />
+            <div className="flex flex-col justify-end items-center h-full relative z-10 pb-4">
+              <img src={logo} alt="logo" className="w-auto h-auto max-w-xs" />
+              <ul className="flex text-xs justify-around items-center pb-5 space-x-4 font-bold text-[#C5C8D0]">
+              <li className="relative ">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2 before:text-green-500">
+                    Soon Up
+                  </span>
+                </li>
+                <li className="relative">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
+                    Romance
+                  </span>
+                </li>
+                <li className="relative">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
+                    Scifi
+                  </span>
+                </li>
+                <li className="relative">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
+                    Musical
+                  </span>
+                </li>
+                <li className="relative bg-[#2B2D34] font-semibold h-[18px] w-[30px] flex justify-center items-center rounded-sm">
+                  <span className="before:content-['•'] before:absolute font-bold before:-left-2 before:top-1/2 before:-translate-y-1/2">
+                    U/A
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Carousel>
+
+        <div className="flex mx-auto w-[320px] text-[14px] h-[48px] gap-2 p-3 mt-1">
+          <div className="flex justify-center items-center w-[260px] h-[48px] bg-[#222227] rounded-md">
+            <img src={notify} className="text-[#C5C8D0] text-md font-bold" />
+            <p className="text-[#C5C8D0] font-semibold ml-2">Notify Me</p>
+          </div>
+          <div className="p-3 bg-[#222227] w-[15vw] h-[48px] flex justify-center items-center text-[#C5C8D0] rounded-md font-bold">
+            <button>
+              <img src={wish} />
+            </button>
+          </div>
+        </div>
+        <br />
+      </div>
+
       <NowOnScreen />
       <TopRated />
       <ScreeningSoon />
@@ -163,6 +176,6 @@ const HomeScreen = () => {
       <GenreSection />
     </>
   );
-}
+};
 
 export default HomeScreen;
